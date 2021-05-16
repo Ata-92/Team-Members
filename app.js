bf4df0730c3c60c7108558d1bc6fc61fba69e856
Ata-92 <atakal92@gmail.com>
2021-05-16 17:19:18 +0300

    Add form-switch class to radio input

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-const wrapper = document.querySelector(".wrapper-off");
-const icon = document.getElementsByTagName("i")[0];
 
-icon.addEventListener("click", () => {
-  icon.classList.toggle("fa-toggle-on");
-  wrapper.classList.toggle("wrapper-on");
-})
+
+
+// const wrapper = document.querySelector(".wrapper-off");
+// const icon = document.getElementsByTagName("i")[0];
+
+// icon.addEventListener("click", () => {
+//   icon.classList.toggle("fa-toggle-on");
+//   wrapper.classList.toggle("wrapper-on");
+// });
+
+// const wrapper = document.querySelector(".wrapper-off");
+// const input = document.querySelector("input");
+
+// input.addEventListener("click", () => {
+//   wrapper.classList.toggle("wrapper-on");
+// });
